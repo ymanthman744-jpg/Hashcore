9811f78de3de29e7fd10cbdf7c76518de3d3ba18
ymanthman <your@email.com>
2026-03-28 23:42:22 +0300

    fix port final

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ app.post("/deposit", (req, res) => {
 });
 
 // تشغيل
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
