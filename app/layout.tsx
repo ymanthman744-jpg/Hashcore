0b741a92d32496049138082681387943286db0f8
ymanthman744-jpg <ymanthman744@gmail.com>
2026-04-12 11:23:01 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,8 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
+export const metadata = {
   title: "Hashcore",
-  description: "Hashcore app",
+  description: "My app",
 };
 
 export default function RootLayout({
@@ -16,9 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
