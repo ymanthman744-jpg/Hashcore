3536ecf906d7a72b9775141f39ac72cea8780aef
ymanthman744-jpg <ymanthman744@gmail.com>
2026-04-12 19:49:23 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div style={{textAlign: "center", marginTop: "50px"}}>
-      <h1>Welcome 🚀</h1>
-      <a href="/login">Go to Login</a>
+      <h1>🚀 الموقع شغال 100%</h1>
+      <p>الداتا بيز مربوطة تمام</p>
     </div>
   );
 }
