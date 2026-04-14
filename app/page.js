50794a75eabffa1ecc73b32882e41c972db61ffb
ymanthman744-jpg <ymanthman744@gmail.com>
2026-04-14 23:04:57 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,14 @@
 export default function Home() {
   return (
-    <div style={{ textAlign: "center", marginTop: "80px" }}>
-      <h1>🚀 مرحباً في Hashcore</h1>
-      <p>منصة تسجيل ودخول احترافية</p>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 Hashcore</h1>
+
+      <p>أهلاً فيك بالموقع</p>
+
+      <div style={{ marginTop: "20px" }}>
+        <a href="/login" style={{ margin: "10px" }}>Login</a>
+        <a href="/register" style={{ margin: "10px" }}>Register</a>
+      </div>
     </div>
   );
 }
