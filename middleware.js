f096207fb8c0a043bddecee264f71052adaced15
ymanthman744-jpg <ymanthman744@gmail.com>
2026-04-15 22:09:06 +0300

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,21 +1,5 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
-  const token = request.cookies.get("token");
-
-  // إذا المستخدم عم يحاول يدخل عالداشبورد
-  if (request.nextUrl.pathname.startsWith("/dashboard")) {
-    if (!token) {
-      // إذا ما في توكن → رجعو لصفحة تسجيل الدخول
-      return NextResponse.redirect(new URL("/login", request.url));
-    }
-  }
-
+export function middleware(request) {
   return NextResponse.next();
 }
-
-// تحديد الصفحات اللي بدنا نحميها
-export const config = {
-  matcher: ["/dashboard"],
-};
