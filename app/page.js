0c32654c6fe71b564a04e658c6f963f8c280f98b
ymanthman744-jpg <ymanthman744@gmail.com>
2026-04-15 21:54:00 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,8 +2,7 @@ export default function Home() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>🚀 Hashcore</h1>
-
-      <p>أهلاً فيك بالموقع</p>
+      <p>أهلاً بك بالموقع</p>
 
       <div style={{ marginTop: "20px" }}>
         <a href="/login" style={{ margin: "10px" }}>Login</a>
